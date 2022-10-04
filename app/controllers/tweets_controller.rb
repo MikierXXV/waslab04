c0780d6796e798d3178e44df9d47222bcb2fa52b
miguel <miguel.gutierrez.jariod@estudiantat.upc.edu>
2022-10-04 22:10:52 +0000

    tasca #6 finalitzada, surt un error amb el metode push, no trobem la solució però el que sobte es que ens funcionava en un principi

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -33,7 +33,6 @@ class TweetsController < ApplicationController
   # POST /tweets or /tweets.json
   def create
     @tweet = Tweet.new(tweet_params)
-
     respond_to do |format|
       if @tweet.save
         format.html { redirect_to tweet_url(@tweet), notice: "Tweet was successfully created." }
